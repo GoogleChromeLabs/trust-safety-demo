1a2c219c9b770f2f885881a13d651d1b7dbe2109
maudnals <maud.nalpas@gmail.com>
2020-09-22 14:09:59 +0200

    feat: formatted referrer diferently when empty

--- referrer/site-two/public/script.js
+++ referrer/site-two/public/script.js
@@ -1,6 +1,6 @@
 function formatReferrer(referrer, policyId) {
   return referrer.replace(
-    /stuff\/detail\?tag=red&p=p[0-2]{1}/g,
+    /stuff\/detail\?tag=red&p=p[0-3]{1}/g,
     `<span class="nok">stuff/detail?tag=red&p=${policyId}</span>`
   )
 }
@@ -12,10 +12,14 @@ const els = [documentReferrerEl, headerReferrerEl]
 
 els.forEach((el) => {
   const referrer = el.innerText
-  const params = new URL(referrer).searchParams
-  const policyId = params.get('p')
-  const formattedReferrer = formatReferrer(referrer, policyId)
-  el.innerHTML = formattedReferrer
+  if (referrer) {
+    const params = new URL(referrer).searchParams
+    const policyId = params.get('p')
+    const formattedReferrer = formatReferrer(referrer, policyId)
+    el.innerHTML = formattedReferrer
+  } else {
+    el.innerText = 'empty (no referrer)'
+  }
 })
 
 const isInIframe = parent !== window
