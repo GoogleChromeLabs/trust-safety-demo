32aaadebddbea4aa62e4553ba7df076a4b5af22f
Rowan Merewood <rowan@merewood.org>
2020-12-18 14:28:26 +0000

    Update tokens to match headers

--- user-agent-client-hints/README.md
+++ user-agent-client-hints/README.md
@@ -13,3 +13,36 @@ User-Agent string to make use of User-Agent Client Hints instead.
 📖 Read the article on <https://web.dev/user-agent-client-hints>
 
 🐦 Ping <https://twitter.com/rowan_m> with questions.
+
+## Viewing the demo
+
+🎏 You can see a running version on Glitch
+<https://user-agent-client-hints.glitch.me/>
+
+\*️⃣ Requires Chrome 84 Beta or equivalent.
+
+🚩 Enable the `chrome://flags/#enable-experimental-web-platform-features` flag
+
+## Running the demo locally
+
+Before the initial run, you will need to download the dependencies from from
+[npm](https://docs.npmjs.com/). In this directory, run:
+
+```
+npm ci
+```
+
+Start the server using:
+
+```
+npm start
+```
+
+The server will run on a random port which will be shown in the console.
+
+If you're developing, then there is a task to start the server in development
+mode on port 8080.
+
+```
+npm run start:dev
+```
--- user-agent-client-hints/server.js
+++ user-agent-client-hints/server.js
@@ -1,40 +1,59 @@
 const _HINTS = [
-  'UA-Arch',
-  'UA-Full-Version',
-  'UA-Mobile',
-  'UA-Model',
-  'UA-Platform-Version',
-  'UA-Platform',
-  'UA',
+  'Sec-CH-UA',
+  'Sec-CH-UA-Mobile',
+  'Sec-CH-UA-Full-Version',
+  'Sec-CH-UA-Platform',
+  'Sec-CH-UA-Platform-Version',
+  'Sec-CH-UA-Arch',
+  'Sec-CH-UA-Model',
 ];
 
 const express = require('express');
 const app = express();
 
+/*
+ * If you need basic templating, you can enable Mustache here
+ * Personal preference is just to "upgrade" existing HTML files with templated variables
+ * Enabling the view cache after the demo is published
+ */
 const mustacheExpress = require('mustache-express');
 app.engine('html', mustacheExpress());
- 
 app.set('view engine', 'html');
 app.set('views', __dirname + '/public');
-app.set('view cache', true);
 
+if (app.get('env') === 'production') {
+  app.set('view cache', true);
+}
 
+// Allow server to run correctly behind a proxy
 app.enable('trust proxy');
+
+/*
+ * Redirect requests to HTTPS by default and set the HSTS header.
+ * You will need to disable or modify this if your demo requires plain HTTP
+ */
 app.use(function (req, res, next) {
+  // Allow http://localhost
+  if (req.secure === false && req.hostname === 'localhost') {
+    return next();
+  }
+
+  // Set the HSTS header if we're already on HTTPS
   if (req.secure) {
     res.set('Strict-Transport-Security', 'max-age=63072000; inlcudeSubdomains; preload');
     return next();
   }
-  
+
+  // Otherwise redirect to HTTPS
   res.redirect(301, 'https://' + req.headers.host + req.url);
 });
 
 app.get('/', (req, res) => {
-  let header = '[not set]';
-  
+  let displayHeader = '[not set]';
+
   if (req.query.noheader != 'noheader') {
     let rawCH = [];
-    
+
     if (typeof req.query.uach === 'string') {
       rawCH = [req.query.uach];
     } else if (Array.isArray(req.query.uach)) {
@@ -49,30 +68,46 @@ app.get('/', (req, res) => {
       }
     });
 
-    res.set('Accept-CH', acceptCH.join(', '));
-    header = 'Accept-CH: ' + acceptCH.join(', ');
+    let mergedTokens = acceptCH.join(', ');
+
+    // Older versions of the spec did not include the 'Sec-CH-' prefix
+    // during the transition, we should send both formats
+    acceptCH.forEach(hint => {
+      mergedTokens += ', ' + hint.substring(7);
+    });
+
+    res.set('Accept-CH', mergedTokens);
+    displayHeader = 'Accept-CH: ' + mergedTokens;
   }
 
-  res.render('index', { header: header });
+  res.render('index', { displayHeader: displayHeader });
 });
 
 app.get('/show-headers.json', (req, res) => {
+  const uachHeaders = {};
+  _HINTS.forEach((hint) => {
+    uachHeaders[hint] = req.get(hint);
+  })
+
   res.json({
     'Accept-CH': res.get('Accept-CH'),
-    'Sec-CH': {
-      'UA-Arch': req.get('Sec-CH-UA-Arch'),
-      'UA-Model': req.get('Sec-CH-UA-Model'),
-      'UA-Platform': req.get('Sec-CH-UA-Platform'),
-      'UA-Platform-Version': req.get('Sec-CH-UA-Platform-Version'),
-      'UA': req.get('Sec-CH-UA'),
-      'UA-Full-Version': req.get('Sec-CH-UA-Full-Version'),
-      'UA-Mobile': req.get('Sec-CH-UA-Mobile'),
-    }
-  });  
+    'Sec-CH-UA': uachHeaders,
+    'User-Agent': req.get('User-Agent')
+  });
 });
 
-app.use(express.static('public', { maxAge: '1d' } ));
+// By default, fall back to serving from the `public` directory
+app.use(express.static('public'));
+
+// Cache static files in production
+if (app.get('env') === 'production') {
+  app.use(express.static('public', { maxAge: '1d' }));
+}
 
 const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+
+  if (app.get('env') === 'development') {
+    console.log('If you are running locally, try http://localhost:' + listener.address().port);
+  }
 });
