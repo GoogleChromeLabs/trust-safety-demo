2f42d75b109041295db6d11f887241c31aa5d311
maudnals <maud.nalpas@gmail.com>
2021-06-11 14:46:09 +0200

    Updated code to new API v2 (naming)

--- conversion-measurement/adtech/static/reports-script.js
+++ conversion-measurement/adtech/static/reports-script.js
@@ -13,7 +13,7 @@ function updateUi(reports) {
     const { credit, date } = report
     const time = new Date(date).toLocaleTimeString()
     const day = new Date(date).toDateString()
-    return `<div><div>${report['impression-data']}</div><div>${report['conversion-data']}</div><div>${credit}</div><div>${time} ${day}</div></div>`
+    return `<div><div>${report['source_event_id']}</div><div>${report['trigger_data']}</div><div>${credit}</div><div>${time} ${day}</div></div>`
   })
   reportListEl.innerHTML = reportsHTML.reduce((acc, c) => `${acc}${c}`, '')
 }
--- conversion-measurement/advertiser/static/demo-steps-checkout.js
+++ conversion-measurement/advertiser/static/demo-steps-checkout.js
@@ -1,6 +1,6 @@
 console.info("💁🏻‍♀️ You've converted your click! Now:")
 console.info(
-  "6. Observe how the conversion took place: open the DevTools Network panel and you'll see that the red pixel 🟥 on this page has sent a request to adtech. On the server, adtech decided this was a conversion, and then responded with something like /.well-known/register-conversion?conversion-data={DATA}. You can see this in the Network panel, in the request's Response Header 'Location'."
+  "6. Observe how the conversion took place: open the DevTools Network panel and you'll see that the red pixel 🟥 on this page has sent a request to adtech. On the server, adtech decided this was a conversion, and then responded with something like /.well-known/attribution-reporting/trigger-attribution?data={DATA}. You can see this in the Network panel, in the request's Response Header 'Location'."
 )
 console.info(
   "7. Now that you've converted, reload chrome://conversion-internals and see how a new conversion report with a conversion data of {DATA} was created."
