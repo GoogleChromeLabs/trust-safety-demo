54992369c76efe7af1392c58cbdc4bd91084ccc2
Robert Kubis <rkubis@google.com>
2022-04-03 23:23:56 -0600

    Improved demo flow and visual instructions

--- conversion-measurement/advertiser/static/toast.js
+++ conversion-measurement/advertiser/static/toast.js
@@ -1,13 +1,14 @@
 if (document.featurePolicy.allowsFeature('attribution-reporting')) {
   Toastify({
-    text: `The conversion pixel makes a request to the adtech endpoint (attributionsrc). \n The adtech endpoint then instructs the browser to trigger an attribution. \n If the browser finds a source event that matches this trigger, it generates an attribution report. \n You can see the report in chrome://attribution-internals.`,
+    text: `<hint> \n The conversion pixel makes a request to the adtech endpoint (attributionsrc). \n The adtech endpoint then instructs the browser to trigger an attribution. \n If the browser finds a source event that matches this trigger, it generates an attribution report. \n You can see the report in chrome://attribution-internals.\n</hint>`,
     duration: -1, // infinity
     close: true,
-    gravity: 'bottom',
+    gravity: 'top',
     position: 'right',
     stopOnFocus: true, // Prevents dismissing of toast on hover
     offset: {
-      y: 60
+      y: 500,
+      x: 40
     },
     style: {
       cursor: 'default',
@@ -15,8 +16,7 @@ if (document.featurePolicy.allowsFeature('attribution-reporting')) {
       cursor: 'pointer',
       fontFamily: "'Lucida Console', Monaco, monospace",
       fontSize: '.8rem',
-      boxShadow:
-        'rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;',
+      boxShadow: 'rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;',
       borderRadius: '6px'
     }
   }).showToast()
