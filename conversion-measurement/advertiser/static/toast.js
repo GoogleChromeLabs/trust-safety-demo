745d798faeae2a7b10d3b72f61046d0623669b3f
Robert Kubis <rkubis@google.com>
2022-04-05 12:04:50 -0600

    added missing adtech endpoint + license headers

--- conversion-measurement/advertiser/static/toast.js
+++ conversion-measurement/advertiser/static/toast.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 if (document.featurePolicy.allowsFeature('attribution-reporting')) {
   Toastify({
     text: `<hint> \n The conversion pixel makes a request to the adtech endpoint (attributionsrc). \n The adtech endpoint then instructs the browser to trigger an attribution. \n If the browser finds a source event that matches this trigger, it generates an attribution report. \n You can see the report in chrome://attribution-internals.\n</hint>`,
