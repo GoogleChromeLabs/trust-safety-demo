2610c42b1c1a9e1213674a266ec84f13a36b997d
Maud <maud.nalpas@gmail.com>
2022-04-07 12:52:46 +0200

    Feat: Various layout fixes (#20)

--- conversion-measurement/advertiser/static/toast.js
+++ conversion-measurement/advertiser/static/toast.js
@@ -16,7 +16,7 @@
 
 if (document.featurePolicy.allowsFeature('attribution-reporting')) {
   Toastify({
-    text: `<hint> \n The conversion pixel makes a request to the adtech endpoint (attributionsrc). \n The adtech endpoint then instructs the browser to trigger an attribution. \n If the browser finds a source event that matches this trigger, it generates an attribution report. \n You can see the report in chrome://attribution-internals.\n</hint>`,
+    text: `💡 Demo tip: \n\n The conversion pixel makes a request to the adtech endpoint (attributionsrc). \n The adtech endpoint then instructs the browser to trigger an attribution. \n If the browser finds a source event that matches this trigger, it generates an attribution report. \n\n You can see the report in chrome://attribution-internals.\n`,
     duration: -1, // infinity
     close: true,
     gravity: 'top',
@@ -27,13 +27,15 @@ if (document.featurePolicy.allowsFeature('attribution-reporting')) {
       x: 40
     },
     style: {
-      cursor: 'default',
       background: '#000000d6',
       cursor: 'pointer',
       fontFamily: "'Lucida Console', Monaco, monospace",
       fontSize: '.8rem',
-      boxShadow: 'rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;',
-      borderRadius: '6px'
+      boxShadow:
+        'rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;',
+      borderRadius: '6px',
+      width: '40vw',
+      maxWidth: '40rem'
     }
   }).showToast()
 }
