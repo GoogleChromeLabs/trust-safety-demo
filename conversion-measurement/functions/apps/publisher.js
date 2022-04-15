9d15a0c3bf0a5128fd368613bb3392a2856e12bd
Robert Kubis <rkubis@google.com>
2022-04-15 11:55:59 -0600

    Migration to multi-site firebase project

--- conversion-measurement/functions/apps/publisher.js
+++ conversion-measurement/functions/apps/publisher.js
@@ -14,24 +14,24 @@
  * limitations under the License.
  */
 
+const functions = require('firebase-functions');
 const express = require('express')
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
 
-const app = express()
-app.set('view engine', 'pug')
-const PORT = 8081
+const publisher = express()
+publisher.set('view engine', 'pug')
+publisher.set('views', './views/publisher')
+
 const demoHomeUrl = process.env.DEMO_HOME_URL
 const publisherUrl = process.env.PUBLISHER_URL
 const advertiserUrl = process.env.ADVERTISER_URL
 const adtechUrl = process.env.ADTECH_URL
 
-app.use(express.static('static'))
-
-app.get('/', (req, res) => {
+publisher.get('/', (req, res) => {
   res.render('index', { demoHomeUrl, publisherUrl, advertiserUrl, adtechUrl })
 })
 
-app.get('/click-element', (req, res) => {
+publisher.get('/click-element', (req, res) => {
   const adScriptUrl = `${process.env.ADTECH_URL}/ad-script-click-element`
   res.render('article', {
     adScriptUrl,
@@ -42,7 +42,7 @@ app.get('/click-element', (req, res) => {
   })
 })
 
-app.get('/click-js', (req, res) => {
+publisher.get('/click-js', (req, res) => {
   const adScriptUrl = `${process.env.ADTECH_URL}/ad-script-click-js`
   res.render('article', {
     adScriptUrl,
@@ -53,7 +53,7 @@ app.get('/click-js', (req, res) => {
   })
 })
 
-app.get('/view-element-img', (req, res) => {
+publisher.get('/view-element-img', (req, res) => {
   const adScriptUrl = `${process.env.ADTECH_URL}/ad-script-view-img`
   res.render('article', {
     adScriptUrl,
@@ -64,7 +64,7 @@ app.get('/view-element-img', (req, res) => {
   })
 })
 
-app.get('/view-js', (req, res) => {
+publisher.get('/view-js', (req, res) => {
   const adScriptUrl = `${process.env.ADTECH_URL}/ad-script-view-js`
   res.render('article', {
     adScriptUrl,
@@ -75,9 +75,4 @@ app.get('/view-js', (req, res) => {
   })
 })
 
-const listener = app.listen(process.env.PORT || PORT, () => {
-  console.log(
-    '\x1b[1;32m%s\x1b[0m',
-    `📰 Publisher server is listening on port ${listener.address().port}`
-  )
-})
+exports.publisher = functions.https.onRequest(publisher);
