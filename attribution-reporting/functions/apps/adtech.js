4d8874ec630a6b9f16cbdade18a03714b9f3b188
Maud <maud.nalpas@gmail.com>
2023-05-03 10:21:54 +0200

    Fix: fixed no-op JS source registration (#45)

--- attribution-reporting/functions/apps/adtech.js
+++ attribution-reporting/functions/apps/adtech.js
@@ -142,11 +142,13 @@ adtech.get('/ad-script-click-js', (req, res) => {
 /*                  Source registration (ad click or view)                    */
 /* -------------------------------------------------------------------------- */
 
-adtech.get(['/register-source-image', '/register-source-href'], (req, res) => {
-  const attributionDestination = process.env.ADVERTISER_URL
-  // For demo purposes, sourceEventId is a random ID. In a real system, this ID would be tied to a unique serving-time identifier mapped to any information an adtech provider may need
-  const sourceEventId = Math.floor(Math.random() * 1000000000000000)
-  const legacyMeasurementCookie = req.cookies['__session']
+adtech.get(
+  ['/register-source-image', '/register-source-href', '/register-source-js'],
+  (req, res) => {
+    const attributionDestination = process.env.ADVERTISER_URL
+    // For demo purposes, sourceEventId is a random ID. In a real system, this ID would be tied to a unique serving-time identifier mapped to any information an adtech provider may need
+    const sourceEventId = Math.floor(Math.random() * 1000000000000000)
+    const legacyMeasurementCookie = req.cookies['__session']
 
   const headerConfig = {
     source_event_id: `${sourceEventId}`,
@@ -174,7 +176,7 @@ adtech.get(['/register-source-image', '/register-source-href'], (req, res) => {
     res.status(200).sendFile('blue-shoes.png', {
       root: path.join(__dirname, '../../sites/adtech')
     })
-  } else if (req.originalUrl === '/register-source-href') {
+  } else if (['/register-source-href', '/register-source-js'].includes(req.originalUrl)) {
     res.redirect(advertiserUrl)
   }
 })
